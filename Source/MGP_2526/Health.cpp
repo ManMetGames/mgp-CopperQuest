f21df4347f87c5ba94bd82784acd05da712bb467
CopperQuest <bcavalcanti2019@hotmail.com>
2026-03-04 11:31:15 +0000

    Added Health script to BP_ThirdPersonCharacter

--- Source/MGP_2526/Health.cpp
+++ Source/MGP_2526/Health.cpp
@@ -16,17 +16,32 @@ UHealth::UHealth()
 void UHealth::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
 	
 }
-
 
 // Called every frame
 void UHealth::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
 
+void UHealth::TakeDamage(int Damage)
+{
+	HP -= Damage;
+	if (HP < 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("I am hit"));
+		HP = 0;
+		// Tells interested classes we have died
+		Died.Broadcast();
+	}
+}
+//allows player to heal
+void UHealth::Heal(int Heal)
+{
+	HP += Heal;
+	if (HP > MaxHealth)
+	{
+		HP = MaxHealth;
+	}
+}
